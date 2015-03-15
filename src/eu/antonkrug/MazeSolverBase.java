9f2cd20d5e46cefb878dc824f4243f3989838e8a
Anton Krug <antonkrug@gmail.com>
2015-03-15 01:25:48 +0000

    sorted

--- src/eu/antonkrug/MazeSolverBase.java
+++ src/eu/antonkrug/MazeSolverBase.java
@@ -67,19 +67,6 @@ public abstract class MazeSolverBase implements MazeSolver {
 	}
 
 	/**
-	 * Simplifying some frequently used operation, if this would be C++ we could
-	 * overload the operator and have peace, but java must be java.
-	 * 
-	 * @return Will return point which will be first point shifted by second point
-	 *         (or vice versa, still same result)
-	 */
-	protected Point pointsTranslate(Point first, Point second) {
-		Point ret = new Point(first);
-		ret.translate(second.x, second.y);
-		return ret;
-	}
-
-	/**
 	 * Will add one or more destinations to maze
 	 * 
 	 * @param destination
@@ -120,104 +107,30 @@ public abstract class MazeSolverBase implements MazeSolver {
 	}
 
 	/**
-	 * Returns measured time between the solver was started, till it found
-	 * solution
+	 * Will add starting position into maze, a maze can contain multiple starting
+	 * positions. And position which will gain the shortest path will choosen.
+	 * 
+	 * @param origin
+	 * @throws Exception
+	 */	
+	@Override
+	abstract public void addStartPosition(Point origin) throws Exception;
+
+	/**
+	 * Returns final solved path
+	 * 
+	 * @return
+	 */	
+	@Override
+	abstract public List<Point> backTracePath();
+
+	/**
+	 * Returns current final path by partially solved search
 	 * 
 	 * @return
 	 */
 	@Override
-	public long timeTaken() {
-		return (timeStop - timeStart) / 1000000;
-	}
-
-	/**
-	 * If solver is finished, do final checks and cleanup
-	 * 
-	 * @return
-	 */
-	@Override
-	public int solveStepFinish() {
-		this.timeStop = System.nanoTime();
-
-		doNotSolveAgain = true;
-
-		if (!destinations.contains(currentStep)) return -1;
-
-		// last step, when destination and current step are the same, we will flag
-		// which destionation we reached
-		markNodeAsVisited(currentStep);
-
-		return 0;
-	}
-
-	/**
-	 * Return flag if the alrgorithm is allowed to see the destination
-	 * 
-	 * @return the destinationVisible
-	 */
-	@Override
-	public boolean isDestinationVisible() {
-		return destinationVisible;
-	}
-
-	/**
-	 * Flag if algorithm is solved (with solution or not) and locked for any new
-	 * solving attempt.
-	 * 
-	 * @return the doNotSolveAgain
-	 */
-	@Override
-	public boolean isDoNotSolveAgain() {
-		return doNotSolveAgain;
-	}
-
-	/**
-	 * Called before solver can do each step
-	 * 
-	 * @return
-	 */
-	@Override
-	public int solveStepInit() {
-		if (origin == null || doNotSolveAgain) {
-			doNotSolveAgain = true;
-			return -1;
-		}
-		this.timeStart = System.nanoTime();
-		currentStep = origin;
-
-		return 0;
-	}
-
-	/**
-	 * Set current step to null
-	 * 
-	 * @return
-	 */
-	@Override
-	public boolean solveStepDidntStarted() {
-		return currentStep == null;
-	}
-
-	/**
-	 * Set all destinations to given list.
-	 * 
-	 * @param destinations
-	 */
-	@Override
-	public void setDestinations(List<Point> destinations) {
-		this.destinations = destinations;
-	}
-
-	/**
-	 * Flag if algorithm is allowed to see destination
-	 * 
-	 * @param destinationVisible
-	 *          the destinationVisible to set
-	 */
-	@Override
-	public void setDestinationVisible(boolean destinationVisible) {
-		this.destinationVisible = destinationVisible;
-	}
+	abstract public List<Point> backTracePathPartially();
 
 	/**
 	 * Evaluates given position with all cardinal directions and then returns the
@@ -227,46 +140,6 @@ public abstract class MazeSolverBase implements MazeSolver {
 	 * @return
 	 */
 	abstract protected Point doOneStep(Point currentPosition);
-
-	/**
-	 * If solveStepCondition() returns true you can do one step iteration
-	 */
-	@Override
-	public void solveStepOneIteration() {
-		currentStep = doOneStep(currentStep);
-	}
-
-	/**
-	 * By default retun for both alternatives null, so then if any implementation
-	 * will override any of them then that one will be called by Gui (gui detects
-	 * which one is the working one by checking for null)
-	 */
-	@Override
-	public Stream<Point> getVisitedAlreadyAlternative() {
-		// by default do not support stream
-		return null;
-	}
-
-	/**
-	 * By default retun for both alternatives null, so then if any implementation
-	 * will override any of them then that one will be called by Gui (gui detects
-	 * which one is the working one by checking for null)
-	 */
-	@Override
-	public Map<Point, Point> getVisitedAlready() {
-		// by default do not support map
-		return null;
-	}
-
-	/**
-	 * Condition which will be checked in each step
-	 * 
-	 * @return
-	 */
-	@Override
-	public boolean solveStepCondition() {
-		return !destinations.contains(currentStep) && getVisitSize() > 0;
-	}
 
 	/**
 	 * Will return Approach of this implementation
@@ -299,6 +172,114 @@ public abstract class MazeSolverBase implements MazeSolver {
 	}
 
 	/**
+	 * Returns open list
+	 * 
+	 * @return the visit
+	 */
+	@Override
+	abstract public Stream<Point> getVisit();
+
+	/**
+	 * By default retun for both alternatives null, so then if any implementation
+	 * will override any of them then that one will be called by Gui (gui detects
+	 * which one is the working one by checking for null)
+	 */
+	@Override
+	public Map<Point, Point> getVisitedAlready() {
+		// by default do not support map
+		return null;
+	}
+
+	/**
+	 * By default retun for both alternatives null, so then if any implementation
+	 * will override any of them then that one will be called by Gui (gui detects
+	 * which one is the working one by checking for null)
+	 */
+	@Override
+	public Stream<Point> getVisitedAlreadyAlternative() {
+		// by default do not support stream
+		return null;
+	}
+
+	/**
+	 * Returns size of the closed list
+	 * 
+	 * @return
+	 */
+	@Override
+	abstract public int getVisitedAlreadySize();
+
+	/**
+	 * Returns open list size
+	 * 
+	 * @return the visit size
+	 */
+	@Override
+	abstract public int getVisitSize();
+
+	/**
+	 * Return flag if the alrgorithm is allowed to see the destination
+	 * 
+	 * @return the destinationVisible
+	 */
+	@Override
+	public boolean isDestinationVisible() {
+		return destinationVisible;
+	}
+
+	/**
+	 * Flag if algorithm is solved (with solution or not) and locked for any new
+	 * solving attempt.
+	 * 
+	 * @return the doNotSolveAgain
+	 */
+	@Override
+	public boolean isDoNotSolveAgain() {
+		return doNotSolveAgain;
+	}
+
+	/**
+	 * Move a position from open list to closed list
+	 * 
+	 * @param index
+	 */
+	abstract protected void markNodeAsVisited(Point index);
+
+	/**
+	 * Simplifying some frequently used operation, if this would be C++ we could
+	 * overload the operator and have peace, but java must be java.
+	 * 
+	 * @return Will return point which will be first point shifted by second point
+	 *         (or vice versa, still same result)
+	 */
+	protected Point pointsTranslate(Point first, Point second) {
+		Point ret = new Point(first);
+		ret.translate(second.x, second.y);
+		return ret;
+	}
+
+	/**
+	 * Set all destinations to given list.
+	 * 
+	 * @param destinations
+	 */
+	@Override
+	public void setDestinations(List<Point> destinations) {
+		this.destinations = destinations;
+	}
+
+	/**
+	 * Flag if algorithm is allowed to see destination
+	 * 
+	 * @param destinationVisible
+	 *          the destinationVisible to set
+	 */
+	@Override
+	public void setDestinationVisible(boolean destinationVisible) {
+		this.destinationVisible = destinationVisible;
+	}
+
+	/**
 	 * Will attempt to find path from start to finish, contains more simpler
 	 * methods, each of them can be overider separetly by each implementation, if
 	 * the algorithm needs something different from this default one
@@ -328,60 +309,79 @@ public abstract class MazeSolverBase implements MazeSolver {
 	}
 
 	/**
-	 * Will add starting position into maze, a maze can contain multiple starting
-	 * positions. And position which will gain the shortest path will choosen.
-	 * 
-	 * @param origin
-	 * @throws Exception
-	 */	
-	@Override
-	abstract public void addStartPosition(Point origin) throws Exception;
-
-	/**
-	 * Returns final solved path
-	 * 
-	 * @return
-	 */	
-	@Override
-	abstract public List<Point> backTracePath();
-
-	/**
-	 * Returns current final path by partially solved search
+	 * Condition which will be checked in each step
 	 * 
 	 * @return
 	 */
 	@Override
-	abstract public List<Point> backTracePathPartially();
+	public boolean solveStepCondition() {
+		return !destinations.contains(currentStep) && getVisitSize() > 0;
+	}
 
 	/**
-	 * Returns open list
-	 * 
-	 * @return the visit
-	 */
-	@Override
-	abstract public Stream<Point> getVisit();
-
-	/**
-	 * Returns open list size
-	 * 
-	 * @return the visit size
-	 */
-	@Override
-	abstract public int getVisitSize();
-
-	/**
-	 * Returns size of the closed list
+	 * Set current step to null
 	 * 
 	 * @return
 	 */
 	@Override
-	abstract public int getVisitedAlreadySize();
+	public boolean solveStepDidntStarted() {
+		return currentStep == null;
+	}
 
 	/**
-	 * Move a position from open list to closed list
+	 * If solver is finished, do final checks and cleanup
 	 * 
-	 * @param index
+	 * @return
 	 */
-	abstract protected void markNodeAsVisited(Point index);
+	@Override
+	public int solveStepFinish() {
+		this.timeStop = System.nanoTime();
+
+		doNotSolveAgain = true;
+
+		if (!destinations.contains(currentStep)) return -1;
+
+		// last step, when destination and current step are the same, we will flag
+		// which destionation we reached
+		markNodeAsVisited(currentStep);
+
+		return 0;
+	}
+
+	/**
+	 * Called before solver can do each step
+	 * 
+	 * @return
+	 */
+	@Override
+	public int solveStepInit() {
+		if (origin == null || doNotSolveAgain) {
+			doNotSolveAgain = true;
+			return -1;
+		}
+		this.timeStart = System.nanoTime();
+		currentStep = origin;
+
+		return 0;
+	}
+
+	/**
+	 * If solveStepCondition() returns true you can do one step iteration
+	 */
+	@Override
+	public void solveStepOneIteration() {
+		currentStep = doOneStep(currentStep);
+	}
+
+	/**
+	 * Returns measured time between the solver was started, till it found
+	 * solution
+	 * 
+	 * @return
+	 */
+	@Override
+	public long timeTaken() {
+		return (timeStop - timeStart) / 1000000;
+	}
 
 }
