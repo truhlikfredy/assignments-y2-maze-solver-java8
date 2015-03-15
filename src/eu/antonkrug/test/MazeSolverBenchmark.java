eed8b225619fbd4f8625880052d21c548b8cd4c2
Anton Krug <antonkrug@gmail.com>
2015-03-15 01:51:34 +0000

    hotovy benchmark

--- src/eu/antonkrug/test/MazeSolverBenchmark.java
+++ src/eu/antonkrug/test/MazeSolverBenchmark.java
@@ -2,15 +2,45 @@ package eu.antonkrug.test;
 
 /**
  * @author Anton Krug
+ * @date 2015/03/01
+ * @version 1.1
+ * @requires Java 8!
  * 
- * Warning this will solve a maze 1000 times, it can take some time,
+ * WARNING this will solve a maze 1000 times, it can take some time!
  * On old core 2 duo 2.4Ghz cpu running JDK 8 on Debain Wheezy it can take 
- * around 20s to finish all benchmarks.
+ * around 120s (2 minutes) to finish all benchmarks.
  * 
  *  Benchmark 1 - medium maze (1 start,1 destination)
  *  Benchmark 2 - smaller (1 start + 1 closed start, 1 destination + 1 closed destination)
  *  Benchmark 3 - tiny (2 reachable starts, 2 rechable destinations)
  *  Benchmark 4 - medium maze (1 start, 1 destination, but no solution)
+ *  
+ *  Results:
+ *  
+ *  System:	Linux - amd64 - 3.2.0-4-amd64 - CPUs: 2
+ *  Java:	Oracle Corporation - Java(TM) SE Runtime Environment - 1.8.0_31-b13
+ *  Memory:	1841823744 - Available: 118247312
+ *  Count:	1000 times repeated benchmark
+ *  ---------------------------------------
+ *  Benchmark 1 ASTAR_CONCURENT_HASHMAP:	14552 ms
+ *  Benchmark 2 ASTAR_CONCURENT_HASHMAP:	2027 ms
+ *  Benchmark 3 ASTAR_CONCURENT_HASHMAP:	5 ms
+ *  Benchmark 4 ASTAR_CONCURENT_HASHMAP:	3423 ms
+ *  
+ *  Benchmark 1 BFS_STACK:	38356 ms
+ *  Benchmark 2 BFS_STACK:	2036 ms
+ *  Benchmark 3 BFS_STACK:	0 ms
+ *  Benchmark 4 BFS_STACK:	10083 ms
+ *  
+ *  Benchmark 1 DFS_STACK:	38545 ms
+ *  Benchmark 2 DFS_STACK:	2059 ms
+ *  Benchmark 3 DFS_STACK:	1 ms
+ *  Benchmark 4 DFS_STACK:	10099 ms
+ *  
+ *  Benchmark 1 ASTAR_HASHMAP:	4385 ms
+ *  Benchmark 2 ASTAR_HASHMAP:	25 ms
+ *  Benchmark 3 ASTAR_HASHMAP:	0 ms
+ *  Benchmark 4 ASTAR_HASHMAP:	1026 ms
  */
 
 import java.util.Properties;
@@ -52,6 +82,7 @@ public class MazeSolverBenchmark {
 
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("");
 		loadMaze("1");
 	}
 
@@ -63,7 +94,7 @@ public class MazeSolverBenchmark {
 
 	private void aStarAproaches(Aproach implementationAproach) throws Exception {
 		totalTime = 0;
-		System.out.print("Benchmark "+benchmarkName+" "+implementationAproach + ":\t");
+		System.out.print("Benchmark " + benchmarkName + " " + implementationAproach + ":\t");
 		for (int count = 0; count < REPEAT_COUNT; count++) {
 			MazeSolver solver;
 
@@ -72,7 +103,7 @@ public class MazeSolverBenchmark {
 				case BFS_STACK:
 					solver = new MazeSolverDFS(maze);
 					break;
-					
+
 				case DFS_STACK:
 					solver = new MazeSolverDFS(maze);
 					break;
@@ -130,7 +161,7 @@ public class MazeSolverBenchmark {
 		loadMaze("4");
 		aStarAproaches(Aproach.BFS_STACK);
 	}
-	
+
 	// @Test
 	// public void koloboke() throws Exception {
 	// aStarAproaches(Aproach.KOLOBOKE);
